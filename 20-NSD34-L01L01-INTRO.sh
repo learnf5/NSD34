d38585827c1376db36becc19e3ea0c3c9cf43c90
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2025-04-17 15:06:39 -0700

    Update 20-NSD34-L01L01-INTRO.sh

--- 20-NSD34-L01L01-INTRO.sh
+++ 20-NSD34-L01L01-INTRO.sh
@@ -10,9 +10,9 @@ PS4='+$(date +"%T.%3N"): '
     sudo ssh nginx chown nginx:nginx /etc/nginx/ssl
     sudo ssh nginx mkdir /etc/nginx/ssl-configs
     sudo ssh nginx rm /etc/nginx/conf.d/default.conf
-    sudo scp /tmp/nsd_files/L1-L1-Intro/hosts_nginx nginx:/etc/hosts
-    sudo scp /tmp/nsd_files/L1-L1-Intro/juice.conf  nginx:/etc/nginx/conf.d/juice.conf
+    sudo scp /tmp/nsd_files/INTRO/hosts_nginx nginx:/etc/hosts
+    sudo scp /tmp/nsd_files/INTRO/juice.conf  nginx:/etc/nginx/conf.d/juice.conf
 
     # update local hosts file
-    sudo mv /tmp/nsd_files/L1-L1-Intro/hosts_jump /etc/hosts
+    sudo mv /tmp/nsd_files/INTRO/hosts_jump /etc/hosts
  
