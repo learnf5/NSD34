280b6667e7960cafbc443d26dbdc98e3cbd3ebd5
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2025-05-02 11:17:17 -0700

    Update 20-NSD34-L01L01-INTRO.sh
    Added new github file steps

--- 20-NSD34-L01L01-INTRO.sh
+++ 20-NSD34-L01L01-INTRO.sh
@@ -3,16 +3,22 @@ set -x
 PS4='+$(date +"%T.%3N"): '
 
 # update lab environment
-# pull files from github, prep directories on nginx and copy files to nginx
-    cd /tmp
-    git clone https://github.com/learnf5/nsd.git nsd_files
+# pull files from github
+    curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/INTRO/juice.conf
+    curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/INTRO/hosts_jump
+    curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/INTRO/hosts_nginx
+
+# prep directories on nginx and copy files to nginx
     sudo ssh nginx mkdir /etc/nginx/ssl
     sudo ssh nginx chown nginx:nginx /etc/nginx/ssl
     sudo ssh nginx mkdir /etc/nginx/ssl-configs
+
+# copy files to nginx
+
     sudo ssh nginx rm /etc/nginx/conf.d/default.conf
     sudo scp /tmp/nsd_files/INTRO/hosts_nginx nginx:/etc/hosts
     sudo scp /tmp/nsd_files/INTRO/juice.conf  nginx:/etc/nginx/conf.d/juice.conf
 
-    # update local hosts file
+# update local hosts file on jump system
     sudo mv /tmp/nsd_files/INTRO/hosts_jump /etc/hosts
  
