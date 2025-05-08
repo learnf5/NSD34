42f8d4b82cc5c4f421d2be23fa039923d069c160
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2025-05-08 06:58:07 -0700

    Update 20-NSD34-L02L01-HTTPS.sh

--- 20-NSD34-L02L01-HTTPS.sh
+++ 20-NSD34-L02L01-HTTPS.sh
@@ -43,14 +43,14 @@ PS4='+$(date +"%T.%3N"): '
     sudo ssh nginx chmod +x /home/student/curl_script.sh
     sudo ssh nginx chown student:student /home/student/curl_script.sh
    #### NEED TO ADD STEP TO LAB to rename SecUp_juice.bak to juice.conf OR just the steps to add what we need to juice.conf
-    sudo scp /tmp/SecUp_juice.conf      nginx:/etc/nginx/conf.d/SecUp_juice.bak
+    sudo scp /tmp/juice_SecUp.conf      nginx:/etc/nginx/conf.d/SecUp_juice.bak
    #### NEED TO ADD STEP TO LAB to rename SecUp_api_server.bak to api_server.conf.conf 
-    sudo scp /tmp/SecUp_api_server.bak       nginx:/etc/nginx/conf.d/SecUp_api_server.bak
+    #sudo scp /tmp/SecUp_api_server.bak       nginx:/etc/nginx/conf.d/SecUp_api_server.bak
   #### NEED TO ADD STEP TO LAB to rename proxy-ssl-params.bak to SecUp-proxy-ssl-params.conf.conf
-    sudo scp /tmp/SecUp_ssl-params.bak       nginx:/etc/nginx/ssl-configs/SecUp_ssl-params.bak
+    sudo scp /tmp/ssl-params-SecUp.conf       nginx:/etc/nginx/ssl-configs/ssl-params-SecUp.bak
   #### NEED TO ADD STEP TO LAB to rename SecUp_proxy-ssl-params.bak to SecUp_proxy-ssl-params.conf 
-   # sudo scp /tmp/nsd_files/HTTPS/proxy-ssl-params.conf nginx:/etc/nginx/ssl-configs/proxy-ssl-params.conf    
-   sudo scp /tmp/SecUp_proxy-ssl-params.bak nginx:/etc/nginx/ssl-configs/SecUp_proxy-ssl-params.bak    
+   # sudo scp /tmp/nsd_files/HTTPS/proxy-ssl-params.bak nginx:/etc/nginx/ssl-configs/proxy-ssl-params.bak    
+   sudo scp /tmp/proxy-ssl-params.bak nginx:/etc/nginx/ssl-configs/proxy-ssl-params.bak    
  
   #### NEED TO ADD STEP TO LAB to rename SecUp_dhparam.pem to dhparam.pem 
   #### PROBABLY don't need this since they will have created this file in part 1 of lab 2
