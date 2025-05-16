962ffa001191d49290c2a383555d32ef6fbb0077
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2025-05-16 12:08:07 -0700

    Update 20-NSD34-L05L01-SECURITY.sh

--- 20-NSD34-L05L01-SECURITY.sh
+++ 20-NSD34-L05L01-SECURITY.sh
@@ -10,48 +10,80 @@ PS4='+$(date +"%T.%3N"): '
     curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/SECURITY/ssl-params.conf
     curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/HTTPS/ssl-params.conf
     curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/HTTPS/proxy-ssl-params.conf
-
+    curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/LIMITS/ssl-params-dashboard.conf
     curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/INTRO/hosts_jump
     curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/INTRO/hosts_nginx
-  
+    curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/HTTPS/dhparam.pem
+
+    curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/CERTS/DASHBOARD/ca-cert-dashboard.crt
+    curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/CERTS/DASHBOARD/www.nginxdashboard.com.crt
+    curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/CERTS/DASHBOARD/www.nginxdashboard.com.key
+
+    curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/CERTS/ca-cert.crt
+    curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/CERTS/www.nginxtraining.com.crt
+    curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/CERTS/www.nginxtraining.com.key
+
+    curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/SCRIPTS/create_certs.sh
+    curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/SCRIPTS/curl_script.sh
+    curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/SCRIPTS/upload.sh
+
+    # prepare nginx directories
     sudo ssh nginx rm /etc/nginx/conf.d/default.conf
     sudo ssh nginx mkdir /etc/nginx/ssl
-    sudo ssh nginx chown --recursive nginx:nginx /etc/nginx/ssl
+    sudo ssh nginx mkdir /etc/nginx/ssl/DASHBOARD
     sudo ssh nginx mkdir /etc/nginx/ssl-configs
     sudo ssh nginx mkdir /home/student/ssl
-    sudo ssh nginx chown --recursive student:student /home/student/ssl
-    sudo scp /tmp/hosts_nginx           nginx:/etc/hosts
-    sudo scp /tmp/api_server.conf       nginx:/etc/nginx/conf.d/api_server.orig
-    sudo scp /tmp/proxy-ssl-params.conf nginx:/etc/nginx/ssl-configs/proxy-ssl-params.conf
-    sudo scp /tmp/dhparam.pem           nginx:/etc/nginx/dhparam.pem
+    sudo ssh nginx mkdir /home/student/ssl/DASHBOARD
+
+    # prepare nginx files
+
+    sudo scp /tmp/hosts_nginx nginx:/etc/hosts
     sudo scp /tmp/api_server.conf       nginx:/etc/nginx/conf.d/api_server.conf
-    sudo scp /tmp/juice.conf            nginx:/etc/nginx/conf.d/juice.conf
-    sudo scp /tmp/ssl-params.conf       nginx:/etc/nginx/ssl-configs/ssl-params.conf
+    sudo scp /tmp/api_server.conf nginx:/etc/nginx/conf.d/api_server.orig
+    sudo scp /tmp/juice.conf nginx:/etc/nginx/conf.d/juice.conf
+    sudo scp /tmp/ssl-params.conf nginx:/etc/nginx/ssl-configs/ssl-params.conf
+    sudo scp /tmp/proxy-ssl-params.conf nginx:/etc/nginx/ssl-configs/proxy-ssl-params.conf
+    sudo scp /tmp/ssl-params-dashboard.conf nginx:/etc/nginx/ssl-configs/ssl-params-dashboard.conf
+    sudo scp /tmp/create_certs.sh nginx:/home/student/ssl
+    sudo scp /tmp/curl_script.sh nginx:/home/student/ssl
+    sudo scp /tmp/upload.sh nginx:/home/student/ssl
+     
+    ###Certificate files in /home/student for www.nginxtraining.com and www.nginxdashboard.com
+    
+    sudo scp /tmp/www.nginxtraining.com.crt nginx:/home/student/ssl
+    sudo scp /tmp/www.nginxtraining.com.key nginx:/home/student/ssl
+    sudo scp /tmp/ca-cert.crt nginx:/home/student/ssl
 
-    # Do we need these files???
-    # curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/HTTPS/juice_SecUp.bak
-    # curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/HTTPS/ssl-params-SecUp.conf
-    # curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/HTTPS/ssl-params-SecUp.conf
-    # curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/HTTPS/proxy-ssl-params.bak
-    # curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/HTTPS/create_certs.sh
-    # curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/HTTPS/curl_script.sh
-    # curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/HTTPS/upload.sh
+    sudo scp /tmp/www.nginxdashboard.com.crt nginx:/home/student/ssl/DASHBOARD
+    sudo scp /tmp/www.nginxdashboard.com.key nginx:/home/student/ssl/DASHBOARD
+    sudo scp /tmp/ca-cert-dashboard.crt nginx:/home/student/ssl/DASHBOARD/ca-cert.crt
+    
+    sudo ssh nginx chown --recursive student:student /home/student/ssl
+    sudo ssh nginx chown --recursive nginx:nginx /etc/nginx/ssl
 
-    # ADD these files back in once run lab ONCE and copy files to github 
-    # config certs for nginx server on nginx host
-    #sudo scp /tmp/ca-cert.crt                nginx:/etc/nginx/ssl/ca-cert.crt
-    #sudo scp /tmp/ca-cert.crt                nginx:/home/student/ssl/ca-cert.crt
-    #sudo scp /tmp/ca-cert.key                nginx:/home/student/ssl/ca-cert.key
-    #sudo scp /tmp/ca-cert.srl                nginx:/home/student/ssl/ca-cert.srl
-    #sudo scp /tmp/www.nginxtraining.com.crt  nginx:/etc/nginx/ssl/www.nginxtraining.com.crt
-    #sudo scp /tmp/www.nginxtraining.com.crt  nginx:/home/student/ssl/www.nginxtraining.com.crt
-    #sudo scp /tmp/www.nginxtraining.com.key  nginx:/etc/nginx/ssl/www.nginxtraining.com.key
-    #sudo scp /tmp/www.nginxtraining.com.key  nginx:/home/student/ssl/www.nginxtraining.com.key
-    #sudo scp /tmp/www.nginxtraining.com.csr  nginx:/home/student/ssl/www.nginxtraining.com.csr
-    #sudo scp /tmp/ca-cert-dashboard.crt      nginx:/etc/nginx/ssl/ca-cert-dashboard.crt
-    #sudo scp /tmp/www.nginxdashboard.com.crt nginx:/etc/nginx/ssl/www.nginxdashboard.com.crt
-    #sudo scp /tmps/www.nginxdashboard.com.key nginx:/etc/nginx/ssl/www.nginxdashboard.com.key
+    ###Certificate files in /etc/nginx/ssl for www.nginxtraining.com and www.nginxdashboard.com
+
+    sudo scp /tmp/www.nginxtraining.com.crt nginx:/etc/nginx/ssl/
+    sudo scp /tmp/www.nginxtraining.com.key nginx:/etc/nginx/ssl/
+    sudo scp /tmp/ca-cert.crt nginx:/etc/nginx/ssl/
+    
+    sudo scp /tmp/www.nginxdashboard.com.crt nginx:/etc/nginx/ssl/DASHBOARD
+    sudo scp /tmp/www.nginxdashboard.com.key nginx:/etc/nginx/ssl/DASHBOARD
+    sudo scp /tmp/ca-cert-dashboard.crt nginx:/etc/nginx/ssl/DASHBOARD/ca-cert.crt
+
+    sudo scp /tmp/dhparam.pem nginx:/etc/nginx/dhparam.pem
+    
+    # if want original api_server.conf file without Dashboard and other things then copy from HTTPS to LIMITS folder and name it api_server.orig 
+    # otherwise it will overwrite the updated file for this lab
+    # api_server.conf file in LIMITS folder does have both location /api & location /dashboard 
+    # which would be a continuation of how it was created at the end of lab 2 but then would have to change one of the steps in lab 3 
+    # because it has them add a new location /api and no /dashboard
+    # sudo scp /tmp/api_server.conf nginx:/etc/nginx/conf.d/api_server.orig
+
+# From Lab 3
+## Might want this file - will have to get juice.conf from LIMITS and then name it juice.PRElimits.conf
+    #sudo scp /tmp/nsd_files/LIMITS/juice.PRElimits nginx:/etc/nginx/conf.d/juice.PRElimits
 
     # update local hosts file
-    sudo mv /tmp/nsd_files/INTRO/hosts_jump /etc/hosts
+    sudo mv /tmp/hosts_jump /etc/hosts
  
