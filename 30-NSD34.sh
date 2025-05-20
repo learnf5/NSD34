cf7673467eaaf5f423dfd4ce2808bc798ba63d0a
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2025-05-20 07:41:32 -0700

    Update 30-NSD34.sh

--- 30-NSD34.sh
+++ 30-NSD34.sh
@@ -1,11 +1,3 @@
-# install 30 day nginx one trial licenses from Salesforce - EXPIRES May 22, 2025
-set +x
-curl --silent --remote-name-all --output-dir /tmp --header "Authorization: token $TOKEN" https://raw.githubusercontent.com/learnf5/eval-reg-keys/main/nginx/EXPIRES-May-22-2025/nginx-one-eval.{crt,key,jwt}
-echo curl --silent --remote-name-all --output-dir /tmp --header "Authorization: token xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" https://raw.githubusercontent.com/learnf5/eval-reg-keys/main/nginx/EXPIRES-May-22-2025/nginx-one-eval.{crt,key,jwt}
-set -x
-until sudo scp /tmp/nginx-one-eval.crt nginx:/etc/ssl/nginx/nginx-repo.crt || (( count++ > 5 )); do sleep 5; done
-until sudo scp /tmp/nginx-one-eval.key nginx:/etc/ssl/nginx/nginx-repo.key || (( count++ > 5 )); do sleep 5; done
-until sudo scp /tmp/nginx-one-eval.jwt nginx:/etc/nginx/license.jwt || (( count++ > 5 )); do sleep 5; done
 
 # restart NGINX
 sudo ssh nginx sudo systemctl stop nginx
