23c3269bafcabea9f0afffd3481b71bd6bfd4940
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2025-05-29 14:17:48 -0700

    Update 20-NSD34-L02L01-HTTPS.sh

--- 20-NSD34-L02L01-HTTPS.sh
+++ 20-NSD34-L02L01-HTTPS.sh
@@ -18,28 +18,34 @@ PS4='+$(date +"%T.%3N"): '
 
 # prepare files on nginx
     sudo ssh nginx rm /etc/nginx/conf.d/default.conf
-    sudo ssh nginx mkdir -p /etc/nginx/ssl/DASHBOARD
-    sudo ssh nginx chown --recursive nginx:nginx /etc/nginx/ssl
+    
+    sudo ssh nginx mkdir /etc/nginx/ssl
+    sudo ssh nginx mkdir /etc/nginx/ssl/DASHBOARD
+
     sudo ssh nginx mkdir /etc/nginx/ssl-configs
-    sudo ssh nginx mkdir -p /home/student/ssl/DASHBOARD
-    sudo ssh nginx chown --recursive student:student /home/student/ssl
+    
+    sudo ssh nginx mkdir /home/student/ssl
+    sudo ssh nginx mkdir /home/student/ssl/DASHBOARD
 
 # copy files to nginx
     sudo scp /tmp/hosts_nginx nginx:/etc/hosts
+    
     sudo scp /tmp/create_certs.sh nginx:/home/student/ssl/create_certs.sh
-    sudo scp /tmp/create_certs.sh nginx:/etc/nginx/ssl/DASHBOARD/create_certs.sh
+    sudo scp /tmp/create_certs.sh nginx:/home/student/ssl/DASHBOARD/create_certs.sh
     sudo ssh nginx chmod +x /home/student/ssl/create_certs.sh
-    sudo ssh nginx chmod +x /etc/nginx/ssl/DASHBOARD/create_certs.sh
-    sudo ssh nginx chown student:student /home/student/ssl/create_certs.sh
+    sudo ssh nginx chmod +x /home/student/ssl/DASHBOARD/create_certs.sh
+        
     sudo scp /tmp/juice.conf nginx:/etc/nginx/conf.d/juice.conf
     sudo scp /tmp/ssl-params.conf nginx:/etc/nginx/ssl-configs/ssl-params.conf
 
-    sudo scp /tmp/curl_script.sh            nginx:/home/student/curl_script.sh
+    sudo scp /tmp/curl_script.sh nginx:/home/student/curl_script.sh
     sudo ssh nginx chmod +x /home/student/curl_script.sh
-    sudo ssh nginx chown student:student /home/student/curl_script.sh
   
     sudo scp /tmp/proxy-ssl-params.bak nginx:/etc/nginx/ssl-configs/proxy-ssl-params.bak    
-    sudo scp /tmp/api_server.conf           nginx:/etc/nginx/SecureUpstreams/api_server.conf      
+ 
+    sudo ssh nginx chown --recursive nginx:nginx /etc/nginx/ssl
+    sudo ssh nginx chown --recursive nginx:nginx /etc/nginx/ssl-configs
+    sudo ssh nginx chown --recursive student:student /home/student/ssl
 
     # update local hosts file
     sudo mv /tmp/hosts_jump /etc/hosts
