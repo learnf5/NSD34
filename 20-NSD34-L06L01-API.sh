c23092c52fcf4a408570e77249b0020b998d4560
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2025-06-06 12:18:21 -0500

    Update 20-NSD34-L06L01-API.sh

--- 20-NSD34-L06L01-API.sh
+++ 20-NSD34-L06L01-API.sh
@@ -15,7 +15,7 @@ PS4='+$(date +"%T.%3N"): '
     curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/API/nginx.conf
     curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/API/quotes.jwt
     curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/API/api_secret.jwk
-    curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/API/LeesQuotes.jwt
+    curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/API/LeesQuote.jwt
 
     curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/CERTS/DASHBOARD/ca-cert-dashboard.crt
     curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/CERTS/DASHBOARD/www.nginxdashboard.com.crt
@@ -43,7 +43,7 @@ PS4='+$(date +"%T.%3N"): '
     sudo scp /tmp/nginx.conf nginx:/etc/nginx/nginx.conf
     sudo scp /tmp/quotes.jwt nginx:/home/student/
     sudo scp /tmp/api_secret.jwk nginx:/home/student/
-    sudo scp /tmp/LeesQuotes.jwt nginx:/home/student/
+    sudo scp /tmp/LeesQuote.jwt nginx:/home/student/
 
 
   ###Certificate files for www.nginxtraining.com and www.nginxdashboard.com
